db7d9412205d56b5f125b065bc7f0f6196ab1bdb
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-05-03 23:08:17 +0300

    Add: branches

--- Git History Viewer/Form1.cs
+++ Git History Viewer/Form1.cs
@@ -14,12 +14,23 @@ namespace Git_History_Viewer
 {
     public partial class Form1 : Form
     {
+        public class CommitMetaData
+        {
+            public CommitMetaData(string sha, int x, int y)
+            {
+                this.sha = sha;
+                this.x = x;
+                this.y = y;
+            }
+            public string sha;
+            public int x;
+            public int y;
+        }
         public Form1()
         {
             InitializeComponent();
         }
 
-        public Repository repo;
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -27,56 +38,62 @@ namespace Git_History_Viewer
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            int x = 800;
-            int y = 300;
+            Repository repo;
+
+            int x = 50;
+            int y = 50;
+            List<CommitMetaData> listCommitMetaData = new List<CommitMetaData>();
+
             using (repo = new Repository(@"C:\Users\anas\Desktop\repo"))
             {
+                var g = e.Graphics;
+
                 foreach (var branch in repo.Branches)
                 {
-                    if (branch.IsCurrentRepositoryHead)
+                    var arr = branch.Commits.ToArray<Commit>();
+
+                    bool checkout = false;
+                    for (int i = arr.Length - 1; i > -1; i--)
                     {
-                        var listEnumerator = branch.Commits.GetEnumerator(); // Get enumerator
-
-                        bool condition = true;
-                        while (condition)
+                        if (listCommitMetaData.Find(c => c.sha == arr[i].Sha) != null)
                         {
-                            var g = e.Graphics;
+                            x += 120;
+                            continue;
+                        }
+
+                        // draw circle
+                        g.DrawEllipse(new Pen(Brushes.Black), x, y, 50, 50);
+
+                        // hash
+                        g.DrawString(arr[i].Sha.Substring(0, 7), Font, Brushes.Black, new PointF(x, y + 60));
 
 
-                            try
+                        if (!checkout)
+                        {
+                            var parent = listCommitMetaData.Find(c => c.sha == arr[i].Parents.First<Commit>().Sha);
+
+                            if (parent != null)
                             {
-                                var dummy = listEnumerator.Current;
-                            }
-                            catch (ArgumentNullException)
-                            {
-                                // draw branch
-                                g.DrawRectangle(new Pen(Brushes.Black), x - 20, y - 70, 90, 50);
-
-                                // branch name
-                                g.DrawString(branch.FriendlyName, Font, Brushes.Black, new PointF(x, y - 55));
-                            }
-
-                            condition = listEnumerator.MoveNext();
-
-                            // draw circle
-                            g.DrawEllipse(new Pen(Brushes.Black), x, y, 50, 50);
-
-                            // hash
-                            g.DrawString(listEnumerator.Current.Sha.Substring(0, 7), Font, Brushes.Black, new PointF(x, y + 60));
-
-                            if (condition)
-                            {
-                                // draw line
-                                x -= 60;
-                                g.DrawLine(new Pen(Brushes.Black), x, y + 25, x + 50, y + 25);
-                                x -= 60;
+                                g.DrawLine(new Pen(Brushes.Black), parent.x + 25, parent.y + 80, x + 20, y - 10);
                             }
 
+                            checkout = true;
+                        }
+
+                        if (i > 0)
+                        {
+                            // draw line
+                            x += 60;
+                            g.DrawLine(new Pen(Brushes.Black), x, y + 25, x + 50, y + 25);
+                            x += 60;
                         }
 
 
-
+                        listCommitMetaData.Add(new CommitMetaData(arr[i].Sha, x, y));
                     }
+
+                    y += 170;
+                    x = 50;
                 }
             }
         }
